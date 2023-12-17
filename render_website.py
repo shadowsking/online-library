db6ce2c2cc724bba07415d957eefef8813c47ce2
shadowsking <shadowsking1295@gmail.com>
2023-12-17 16:14:44 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -7,7 +7,7 @@ from livereload import Server
 from more_itertools import chunked
 
 
-def on_reload(folder=None):
+def on_reload(folder=None, pages_folder=None):
     with open(os.path.join(folder, "books.json"), "r") as f:
         books = json.load(f)
 
@@ -16,13 +16,13 @@ def on_reload(folder=None):
         autoescape=select_autoescape(["html", "xml"])
     )
     template = env.get_template("template.html")
-    rendered_page = template.render(
-        iter_books=chunked(books, 2)
-    )
-    with open("index.html", "w", encoding="utf8") as file:
-        file.write(rendered_page)
-
-    print("reloaded")
+    for index, iter_books in enumerate(chunked(books, 20)):
+        rendered_page = template.render(
+            iter_books=chunked(iter_books, 2)
+        )
+        page_path = os.path.join(pages_folder, f"index{index + 1}.html")
+        with open(page_path, "w", encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 def main():
@@ -34,12 +34,18 @@ def main():
         help="path to the catalog with the parsing results",
         default="books_details",
     )
+    parser.add_argument(
+        "--pages_folder",
+        help="path to the catalog with the parsing results",
+        default="pages",
+    )
     args = parser.parse_args()
+    os.makedirs(args.pages_folder, exist_ok=True)
 
-    on_reload(args.dest_folder)
+    on_reload(args.dest_folder, args.pages_folder)
     server = Server()
-    server.watch("template.html", lambda: on_reload(args.dest_folder))
-    server.serve(root='.')
+    server.watch("template.html", lambda: on_reload(args.dest_folder, args.pages_folder))
+    server.serve(root=os.path.join(args.pages_folder, "index1.html"))
 
 
 if __name__ == '__main__':
